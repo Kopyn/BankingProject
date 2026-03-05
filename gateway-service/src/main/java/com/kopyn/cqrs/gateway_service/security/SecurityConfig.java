8c207c81c3c2697921e1ecebf5e4129ec7beeb06
Kopyn <mackopin@gmail.com>
2026-03-05 14:22:05 +0100

    gateway routes

--- gateway-service/src/main/java/com/kopyn/cqrs/gateway_service/security/SecurityConfig.java
+++ gateway-service/src/main/java/com/kopyn/cqrs/gateway_service/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.kopyn.cqrs.gateway_service.security;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,6 +11,8 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+import static org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers.pathMatchers;
+
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
@@ -17,7 +20,9 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
-                // ...
+                .authorizeExchange(exchange -> exchange
+                    .pathMatchers("/actuator/**").permitAll()
+                        .anyExchange().permitAll())
                 .oauth2Login(Customizer.withDefaults());
         return http.build();
     }
