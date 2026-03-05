2879eb1c37917377bac582847283adc84457c94d
Kopyn <mackopin@gmail.com>
2026-03-05 12:38:46 +0100

    spring cloud gateway is up and close to set up as oauth2 client

---
+++ gateway-service/src/main/java/com/kopyn/cqrs/gateway_service/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.kopyn.cqrs.gateway_service.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                // ...
+                .oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
+
+}
