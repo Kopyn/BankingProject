367a28c98d2463a4fa56bec22c1b20bd23e924de
Kopyn <mackopin@gmail.com>
2026-03-09 17:16:52 +0100

    release-3 first commit

--- gateway-service/src/main/java/com/kopyn/cqrs/gateway_service/security/SecurityConfig.java
+++ gateway-service/src/main/java/com/kopyn/cqrs/gateway_service/security/SecurityConfig.java
@@ -4,15 +4,12 @@ import org.springframework.boot.actuate.autoconfigure.security.reactive.Endpoint
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Profile("default")
 @Configuration
-//@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
